c8be7da9cff49e1581972c8e2b9d87e10cc5a355
bryanstark1 <bryandstark@icloud.com>
2023-06-20 16:11:00 -0400

    Flights/new page redners successfully

--- controllers/flights.js
+++ controllers/flights.js
@@ -2,12 +2,15 @@ const Flight = require('../models/flight');
 
 const index = async (req, res) => {
   const flights = await Flight.find({});
-  res.render('flights/index', {flights, title: 'All Flights'});
+  res.render('flights/index', {flights, title: 'Flights List'});
 };
 
-
+const newFlight = (req, res) => {
+  res.render('flights/new', {errorMsg: '', title: 'Add New Flight'});
+};
 
 
 module.exports = {
   index,
+  new: newFlight
 }
